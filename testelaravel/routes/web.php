2931b820dc7ae295128e8dd7df1b787b4e6b0665
guinhomoraes <guilhermeguilherme98@gmail.com>
2018-06-07 13:39:30 -0300

    Inserção de rotas e controllers

--- testelaravel/routes/web.php
+++ testelaravel/routes/web.php
@@ -1,24 +1,21 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return redirect()->route('contato');
 });
 
-Route::get('/empresa', function () {
-    return view('empresa');
+
+
+Route::group(['prefix' => "panel"], function(){
+	Route::get('/empresa', function () {
+   		 return view('empresa');
+	});
+
+	Route::get('/contato', function () {
+	     return view('contato');
+	})->name('contato');
 });
 
-Route::get('/contato', function () {
-    return view('contato');
-});
+
+Route::get('/teste', 'SiteController@index');
